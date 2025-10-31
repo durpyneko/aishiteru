c2239f0c49aed661e344f518c430a7a0a15a6b5c
durpy <89787577+durpyneko@users.noreply.github.com>
2025-10-31 22:24:05 +0200

    add: import components.css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-	import '../app.css';
-	import Header from '$lib/components/header.svelte';
-	// import favicon from '$lib/assets/favicon.svg';
-	
-	let { children } = $props();
+  import "../app.css";
+  import "../components.css";
+  import Header from "$lib/components/header.svelte";
+  // import favicon from '$lib/assets/favicon.svg';
+
+  let { children } = $props();
 </script>
 
 <Header title="home" />
